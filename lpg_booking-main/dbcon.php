f8f65be972e6909234550224b8fbf7be611c973d
chitta kushal <111438028+chittakushal@users.noreply.github.com>
2024-09-04 19:38:23 +0530

    Add files via upload

---
+++ lpg_booking-main/dbcon.php
@@ -0,0 +1,13 @@
+<?php
+
+$con=mysqli_connect('localhost','root','','lpggas');
+if(!$con)
+{
+    die("not connected").mysqli_connect_error();
+}
+
+?>
+<!-- 
+ mysqli_connect() function require 4 parameters 1. host name..2. database username..
+3. database password  ..4. database name -->
+
